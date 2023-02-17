b9be5975bcbb6956bf5a4764ed7e4391961c8392
Hukasx0 <hubertkasp13@gmail.com>
2023-02-17 20:22:44 +0100

    Added http and https proxy

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const http = require('http');
+const https = require('https');
+const url = require('url');
 const app = express();
 
+const host = 'localhost';
 const port = 1337;
 
 const storage = multer.diskStorage({
@@ -16,6 +20,60 @@ const storage = multer.diskStorage({
 
 app.get('/', (req, res) => {
     res.send("Hello world!");
+});
+
+app.get('/httpp/*', (req, res) => {
+    const npurl = "http://"+req.params[0];
+    const purl = url.parse(npurl);
+    const options = {
+        host: purl.hostname,
+        path: purl.path,
+        headers: {
+            'User-Agent': 'example'
+        }
+    };
+    http.get(options, (response) => {
+        if (response.statusCode >= 300 && response.statusCode < 400){
+            const repRedirectUrl = (response.headers.location).replace("http://", '');
+            res.redirect(`http://${host}:${port}/httpp/`+repRedirectUrl);
+        }
+        else{
+            let data = '';
+            response.on('data', (chunk) => {
+                data+=chunk;
+            });
+            response.on('end', () => {
+                res.send(data);
+            });
+        }
+    });
+});
+
+app.get('/httpps/*', (req, res) => {
+    const npurl = "https://"+req.params[0];
+    const purl = url.parse(npurl);
+    const options = {
+        host: purl.hostname,
+        path: purl.path,
+        headers: {
+            'User-Agent': 'example'
+        }
+    };
+    https.get(options, (response) => {
+        if (response.statusCode >= 300 && response.statusCode < 400){
+            const repRedirectUrl = (response.headers.location).replace("https://", '');
+            res.redirect(`http://${host}:${port}/httpps/`+repRedirectUrl);
+        }
+        else{
+            let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+        response.on('end', () => {
+            res.send(data);
+        });
+        }
+    });
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
@@ -34,5 +92,5 @@ app.get('/download/:fileName', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+    console.log(`server is running on port http://${host}:${port}/`);
 });
