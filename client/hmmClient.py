46e292b0aa559d8350f5e4a4595027ed36d0ca3b
Hukasx0 <hubertkasp13@gmail.com>
2023-03-02 09:18:42 +0100

    Added client shell, help menus and autocompletion

--- client/hmmClient.py
+++ client/hmmClient.py
@@ -2,6 +2,7 @@ import sys
 # pip install -r requirements.txt
 import requests
 import pyperclip
+import readline
 
 hmmAddr = ''
 
@@ -14,6 +15,11 @@ def tasks(i):
         print (requests.post(hmmAddr+"/api/task/run", data={"name": i[2]}).text)
     elif i[1] == "del":
         print (requests.get(hmmAddr+"/api/task/del/"+i[2]).text)
+    elif i[1] == "log":
+        with open("tasks.json", 'wb') as file:
+            file.write(requests.get(hmmAddr+"/api/task/log").content)
+    elif i[1] == "help":
+        print ("tasks\ntasks add <taskname> <http/https/httptxt/httpstxt/> <target>\ntasks run <taskname>\ntasks del <taskname>\ntasks help")
 
 def routine(i):
     if len(i) <= 1:
@@ -22,17 +28,21 @@ def routine(i):
         print (requests.post(hmmAddr+"/api/task/interval/add", data={"name": i[2], "time": i[3]}).text)
     elif i[1] == "kill":
         print (requests.get(hmmAddr+"/api/task/interval/kill/"+i[2]).text)
-
+    elif i[1] == "help":
+        print ("routine\nroutine add <taskname> <time>\nroutine <id>\nrotine help")
+        
 def clip(i):
     if len(i) <= 1:
          pyperclip.copy((requests.get(hmmAddr+"/api/clip").text))
+    elif i[1] == "help":
+        print ("clip\nclip history\nclip erase\nclip <text>")
     elif i[1] == "history":
         print (requests.get(hmmAddr+"/api/clip/history").text)
     elif i[1] == "erase":
         print (requests.get(hmmAddr+"/api/clip/erase").text)
     elif len(i) > 1:
         print (requests.post(hmmAddr+"/api/clip/save", data={"data": i[1]}).text)
-
+        
 def notes(i):
     if len(i) <= 1:
         print (requests.get(hmmAddr+"/api/notes").text)
@@ -40,19 +50,25 @@ def notes(i):
         print (requests.post(hmmAddr+"/api/notes/add", data={"name": i[2], "text": i[3], "date": i[4]}).text)
     elif i[1] == "del":
         print (requests.get(hmmAddr+"/api/notes/del/"+i[2]).text)
+    elif i[1] == "help":
+        print ("notes\nnotes add <name> <text> <date>\nnotes del <name>\nnotes help")
 
 def upload(i):
     if i[1] == "link":
         print (requests.post(hmmAddr+"/api/uploadLink", data={"link": i[2]}).text)
+    elif i[1] == "help":
+        print ("upload link <https://example.com>\nupload <filePath>\nupload help")
     elif len(i)>1:
         with open(i[1], 'rb') as file:
             response = requests.post(hmmAddr+"/api/upload", files={'file': file})
-
+            
 def download(i):
-    if len(i)>1:
+    if i[1] == "help":
+        print("download <serverFilePath>\ndownload help")
+    elif len(i)>1:
             with open(i[1], 'wb') as file:
                 file.write(requests.get(hmmAddr+"/api/download/"+i[1]).content)
-
+                
 def scrapper(i):
     if i[1] == "links":
         print (requests.get(hmmAddr+"/api/scraper/links?link="+i[2]).text)
@@ -60,13 +76,18 @@ def scrapper(i):
         print (requests.get(hmmAddr+"/api/scraper/imgs?link="+i[2]).text)
     elif i[1] == "cheerio":
         print (requests.get(hmmAddr+"/api/scraper/cheeriohtml?link="+i[2]+"&parse="+i[3]).text)
-
+    elif i[1] == "help":
+        print("scrapper links <https://example.com>\nscrapper imgs <https://example.com>\nscrapper cheerio <https://example.com> <what you want to scrap, example: div.article>")
+        
 def files(i):
     print (requests.get(hmmAddr+"/api/files/list").text)
-
+    
 def ex(i):
     quit()
 
+def helpm(i):
+    print ("Home Middleman Client\navailable commands:\nhelp\ntasks\nroutine\nclip\nnotes\nupload\ndownload\nscrapper\nproxy\nfiles\nexit")
+    
 def httpp():
     return "/api/httpp/"
 
@@ -99,6 +120,7 @@ apis = {
     "scrapper": scrapper,
     "proxy": proxy,
     "files": files,
+    "help": helpm,
     "exit": ex
 }
 
@@ -117,6 +139,17 @@ def splitWithoutQuo(txt, delimiter=' '):
     result.append(current_word.strip('"'))
     return result
 
+def autoComplete(txt, state):
+    options = ['tasks', 'routine', 'clip', 'notes', 'upload', 'download', 'scrapper','proxy','files','help','exit', 'add','http','httptxt','https','httpstxt','del','run','log','link','erase','links','imgs','cheerio']
+    matches = [option for option in options if option.startswith(txt)]
+    try:
+        return matches[state]
+    except IndexError:
+        return none
+
+readline.set_completer(autoComplete)
+readline.parse_and_bind('tab: complete')
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print ("[i] Usage: python3 hmmClient.py http://<host>:<port>")
@@ -127,4 +160,7 @@ if __name__ == "__main__":
                 continue
             else:
                 hmmAddr = sys.argv[len(sys.argv)-1]
-                apis[i[0]](i)
+                try:
+                    apis[i[0]](i)
+                except KeyError:
+                    continue
